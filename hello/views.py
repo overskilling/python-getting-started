503e966322e5a4d79f077c71c13c6ae20e05ee19
overskilling <neil.skilling@gmail.com>
2022-05-24 21:29:06 -0400

    Back to original

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+
 
 def db(request):
 
